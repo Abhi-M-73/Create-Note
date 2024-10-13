fa6e017c2e32b65204a1ca4946cf573eae3bf6be
Abhishek <abhishekmaddheshiya632@gmail.com>
2024-10-13 22:57:27 +0530

    edit and delete notes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 });
 
 //create a notes
-app.post('/create', function (req, res) {
+app.post('/create', (req, res) => {
     const fileName = req.body.title.split(' ').join('');
     const fileContent = req.body.description;
 
@@ -33,6 +33,7 @@ app.post('/create', function (req, res) {
     });
 });
 
+// show notes 
 app.get('/file/:filename', (req, res) => {
     const filename = req.params.filename;
 
@@ -41,24 +42,38 @@ app.get('/file/:filename', (req, res) => {
             console.log(err);
         }
         res.render("show", {filename: filename, filedata: filedata });
-    })
-});  
+    });
+}); 
 
-// //Delete a notes
-// app.get("/delete", (req, res) => {
-//     const fileName = req.body.title; // Getting the file name from query params
+//edit file redering
+app.get('/edit/:filename', (req, res) => {
+    const filename = req.params.filename;
+    res.render("edit", {filename: filename});
+});
 
-//     // Deleting the file
-//     fs.unlink(`./files/${fileName}`, (err) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//         res.redirect("/"); // Redirect after successful deletion
-//     });
-// });
+//edit file name
+app.post('/edit' , (req, res) => {
+    const previous = req.body.previous;
+    const newFile = req.body.new;
+    fs.rename(`./files/${previous}`,`./files/${newFile}`, (err)=>{
+        if(err){
+            console.log(err);
+        }
+        res.redirect("/");
+    }); 
+});
 
 
-
+//Delete Notes
+app.get('/delete/:filename', (req, res)=> {
+    const fileName = req.params.filename;
+    fs.unlink(`./files/${fileName}`, (err)=> {
+        if(err){
+            console.log(err);
+        }
+        res.redirect("/");
+    });
+});
 
 
 app.listen(3000);
